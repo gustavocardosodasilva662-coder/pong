dcc4c5c330916bebafecf0024a2c70e66dbd6d08
Bryan Pinheiro <bryansouza_01@outlook.com>
2026-09-05 21:54:07 -0300

    add pause

--- script/bot.gd
+++ script/bot.gd
@@ -4,13 +4,16 @@ extends CharacterBody2D
 @onready var ball: CharacterBody2D = $"../Ball"
 
 func _physics_process(delta: float) -> void:
-	# ajustar a velocidade do bot em algum momento para dar chance
 	var ball_direction = global_position.direction_to(ball.global_position)
 	
+	var speed = 400
+	var random = randi_range(1, 100)
+	if random <= 5:
+		speed = 0
+	
 	if ball_direction.y >= 0:
-		velocity.y = 400
+		velocity.y = lerpf(velocity.y, speed, 6 * delta)
 	else:
-		velocity.y = -400
+		velocity.y = lerpf(velocity.y, speed * -1, 6 * delta)
 		
-	print(ball_direction)
 	move_and_slide()
--- script/main.gd
+++ script/main.gd
@@ -7,15 +7,17 @@ var score_bot: int = 0
 @onready var player_score_sound: AudioStreamPlayer = $PlayerScoreSound
 @onready var bot_score_sound: AudioStreamPlayer = $BotScoreSound
 
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("special"):
+		get_tree().paused = true
+
 func _on_goal_one_body_entered(body: Node2D) -> void:
-	print(body)
 	body.global_position = Vector2(463, 307)
 	score1 += 1
 	update_score()
 	bot_score_sound.play()
 
 func _on_goal_two_body_entered(body: Node2D) -> void:
-	print(body)
 	body.global_position = Vector2(463, 307)
 	score_bot += 1
 	update_score()
